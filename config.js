ea255e1d3a1337677803e55dfc613a14796692bd
situmawonderful-beep <situmawonderful@gmail.com>
2026-05-08 11:51:15 +0300

    Add social media links to client config

--- config.js
+++ config.js
@@ -17,6 +17,13 @@ const CONFIG = {
     whatsapp: '254115506649',
     email:    '',
   },
+  // SOCIALS
+  social: {
+  instagram: 'https://www.instagram.com/w.a.n.d.a_c?igsh=MW45bGQ2cGRveWllcw==',
+  tiktok:    'https://www.tiktok.com/@castro._w?_r=1&_t=ZS-96BXVfyoa96',
+  facebook:  'https://www.facebook.com/lavenderglowbeauty',
+  whatsapp:  '254115506649',
+},
 
   // ============================
   // OPENING HOURS
